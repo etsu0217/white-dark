14053737ce795aa210cf9a78c1fb8dc0de7f9e5f
etsu0217 <loverock29690217@gmail.com>
2025-02-01 14:30:25 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// 条件分岐
+var num = 20;
+if (num == 10) { // 条件A
+ console.log("条件Aの処理");
+ console.log(num);
+} else if (num == 20) { // 条件B
+ console.log("条件Bの処理");
+ console.log(num);
+} else { // 条件Aと条件Bに当てはまらなかった時の処理
+ console.log("条件Aと条件以外の処理");
+};
+
+// 課題
+var num = 20;
+if (num == 10) { // 条件A
+ console.log("条件Aの処理");
+ console.log(num);
+} else if (num == 20) { // 条件B
+ console.log("条件Bの処理");
+ console.log(num);
+} else { // 条件Aと条件Bに当てはまらなかった時の処理
+ console.log("条件Aと条件以外の処理");
+};
+
